4193db78facd85f470da6674c4b5515105b3a429
JenHLee <hj.jennie.lee@gmail.com>
2022-08-20 17:08:24 -0600

    modify function.js, create class.js

--- javascript/function.js
+++ javascript/function.js
@@ -184,3 +184,25 @@ const add = (a, b) => a + b;
 (function hello() {
   console.log("IIFE");
 })();
+
+// Quiz function calculate (command, a, b)
+// command: add, substract, divide, multiply, remainder
+
+function calculate(command, a, b) {
+  switch (command) {
+    case "add":
+      return a + b;
+    case "substact":
+      return a - b;
+    case "divide":
+      return a / b;
+    case "multiply":
+      return a * b;
+    case "remainder":
+      return a % b;
+    default:
+      throw Error("unknown command");
+  }
+}
+
+console.log(calculate("add", 2, 3));
