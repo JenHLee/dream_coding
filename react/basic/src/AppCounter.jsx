4dd4de888a1e0e2e2294bbe3643cc89d4c53d07c
JenHLee <hj.jennie.lee@gmail.com>
2022-12-27 00:58:15 -0700

    created Counter component to use useState

---
+++ react/basic/src/AppCounter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Counter from "./components/Counter";
+
+function AppCounter() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+export default AppCounter;
