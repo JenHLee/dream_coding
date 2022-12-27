53df32794d27e5f22a68a099af3f3e3b6a7d1a2d
JenHLee <hj.jennie.lee@gmail.com>
2022-12-26 22:40:35 -0700

    added props in profile component

--- react/basic/src/AppProfile.jsx
+++ react/basic/src/AppProfile.jsx
@@ -1,13 +1,24 @@
-
 import "./App.css";
-import Profile from './components/Profile'
+import Profile from "./components/Profile";
 
 function AppProfile() {
   return (
     <>
-    <Profile />
-    <Profile />
-    <Profile />
+      <Profile
+        image="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+        name="James Smith"
+        title="Front-end Developer"
+      />
+      <Profile 
+        image="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+        name="Anna Young"
+        title="Back-end Developer"
+      />
+      <Profile 
+        image="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        name="Bob Yu"
+        title="Full-Stack Developer"
+      />
     </>
   );
 }
--- react/basic/src/components/Profile.jsx
+++ react/basic/src/components/Profile.jsx
@@ -1,17 +1,12 @@
-import React from 'react'
+import React from "react";
 import "../App.css";
 
-export default function Profile() {
+export default function Profile({ image, name, title }) {
   return (
-    <div className='profile'>
-      <img
-      className='photo'
-      src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
-      alt='avatar'
-      />
-      <h2>James Smith</h2>
-      <p> Full-Stack Developer</p>
+    <div className="profile">
+      <img className="photo" src={image} alt="avatar" />
+      <h2>{name}</h2>
+      <p>{title}</p>
     </div>
-  )
+  );
 }
-
