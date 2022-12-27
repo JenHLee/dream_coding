55b773b7f0e6022aedaffebb3853ccd8279b832b
JenHLee <hj.jennie.lee@gmail.com>
2022-12-27 00:26:20 -0700

    created avatar component with props

--- react/basic/src/components/Profile.jsx
+++ react/basic/src/components/Profile.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Avatar from "./Avatar";
 import "../App.css";
 
 export default function Profile({ image, name, title, isNew }) {
   return (
     <div className="profile">
-      <img className="photo" src={image} alt="avatar" />
-      { isNew && <span className="new">New</span> } 
+      <Avatar image={image}
+      isNew={isNew}/>
       <h2>{name}</h2>
       <p>{title}</p>
     </div>
